21019aabd766862a7aa470f0ceb7ef30a59436b4
vflores <valerie.flores64@gmail.com>
2018-09-09 15:17:32 -0700

    initiated files, created mysql database, and package.json files

---
+++ bamazon.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE product (
+	item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(100) NOT NULL,
+	price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT(100) NOT NULL,
+	PRIMARY KEY(item_id)
+);
+
+INSERT INTO product SET product_name="Cashmere Sweater", department_name="Clothing", price=50.00, stock_quantity="10";
+INSERT INTO product SET product_name="Bicycle", department_name="Sporting Goods", price=75.50, stock_quantity="55";
+INSERT INTO product SET product_name="Dishware Set", department_name="Home Furnishings", price=250.70, stock_quantity="5";
+INSERT INTO product SET product_name="Canoe", department_name="Sporting Goods", price=100.00, stock_quantity="250";
+INSERT INTO product SET product_name="Playstation 4", department_name="Games/Entertainment", price=375.00, stock_quantity="2";
+INSERT INTO product SET product_name="Puppy Chew Toy", department_name="Pet Supplies", price=5.99, stock_quantity="300";
+INSERT INTO product SET product_name="iPhone 10", department_name="Electronics", price=900.00, stock_quantity="1";
+INSERT INTO product SET product_name="Diskman", department_name="Antiques", price=1.45, stock_quantity="500";
+INSERT INTO product SET product_name="Lego Set", department_name="Toys", price=25.00, stock_quantity="756";
+INSERT INTO product SET product_name="Guess Jeans", department_name="Clothing", price=150.00, stock_quantity="689";
+
+SELECT * FROM product;
---
+++ bamazonCustomer.js
@@ -0,0 +1,60 @@
+var mysql = require("mysql");
+var inquirer = require ("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon_db"
+  });
+
+console.log("Welcome to Bamazon!" + "\nPlease wait while available items load...");
+
+connection.connect(function(err) {
+    if (err) throw err;
+    runInventory();
+  });
+
+function runInventory() {
+  connection.query("SELECT * FROM product", function(err, res) {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < res.length; i++) {
+              choiceArray.push(res[i].product_name);
+            }
+            return choiceArray;
+          },
+          message: "Please enter the id number of the item you would like to purchase:"
+        },
+        {
+          name: "purchase",
+          type: "input",
+          message: "Please enter the quantity you would like to purchase:",
+          validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            console.log(" <-- PLEASE INPUT NUMERICAL VALUES ONLY")
+            return false;
+          }
+        }
+      ]).then(function(answer){
+        var chosenItem;
+        for (var i = 0; i < res.length; i++) {
+          if (res[i].item_name === answer.choice) {
+            chosenItem = res[i];
+          }
+        }
+        if (chosenItem.stock_quantity < parseInt(answer.purchase)) {
+          console.log("Insufficient quantity!  Please make another selection.");
+        }
+      });
+  });
+};
